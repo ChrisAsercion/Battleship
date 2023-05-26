afa595bcc3b46d17f557b26e40964159be7dd641
Chris Asercion <chris.asercion@gmail.com>
2023-05-25 18:39:48 -0600

    Adds working hit and health methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,21 +1,24 @@
 class Ship
-  attr_reader :name, :length, :health
+  attr_reader :name, :length, :health, :hit_counter
 
   def initialize(name, length)
     @name = name
     @length = length
-    @health = length
+    @hit_counter = 0
   end
 
   def sunk?
-    if @health == 0
+    if @life_points == 0
       true
     else
       false
     end
   end
+  def health
+    @life_points = @length - @hit_counter
+  end
 
   def hit
-    
+    @hit_counter += 1
   end
 end
