2888804c731ac369cdcf1d858a72a68eb56da559
Chris Asercion <chris.asercion@gmail.com>
2023-05-25 20:46:54 -0600

    Adds render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,11 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @damaged = false
+    @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -19,15 +21,30 @@ class Cell
   end
 
   def fired_upon?
-    if @damaged == false
-      false
-    else
+    if @damaged == true
       true
+    else
+      false
     end
   end
 
   def fire_upon
-    @ship.hit
+    @ship.hit unless @ship == nil
     @damaged = true
+    @fired_upon = true
+  end
+
+  def render(show = false)
+    if show == true 
+      "S"
+    elsif @fired_upon == false
+      "."
+    elsif @fired_upon == true && @ship == nil
+      "M"
+    elsif @ship.sunk? == true
+      "X"
+    else @fired_upon == true && @ship != nil
+      "H"
+    end
   end
 end
--- lib/ship.rb
+++ lib/ship.rb
@@ -14,6 +14,7 @@ class Ship
       false
     end
   end
+  
   def health
     @life_points = @length - @hit_counter
   end
