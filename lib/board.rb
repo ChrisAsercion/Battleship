1390ca5b47f74501276c43ee46cc7e828c1b7b1c
Chris Asercion <chris.asercion@gmail.com>
2023-05-26 18:46:11 -0600

    Add a very clunky method

--- lib/board.rb
+++ lib/board.rb
@@ -5,11 +5,13 @@ require './lib/cell'
 class Board
 def initialize
   @game_board = self.cells
+  @valid_submarine = [['A1', 'A2'], ['A2', 'A3'], ['A3', 'A4'], ['B1', 'B2'], ['B2', 'B3'], ['B3', 'B4'], ['C1', 'C2'], ['C2', 'C3'], ['C3', 'C4'], ['D1', 'D2'], ['D2', 'D3'], ['D3', 'D4'], ['A1', 'B1'], ['A2', 'B2'], ['A3', 'B3'], ['A4', 'B4'], ['B1', 'C1'], ['B2', 'C2'], ['B3', 'C3'], ['B4', 'C4'], ['C1', 'D1'], ['C2', 'D2'], ['C3', 'D3'], ['C4', 'D4']]
+  @valid_cruiser = [['A1', 'A2', 'A3'], ['A2', 'A3', 'A4'], ['B1', 'B2', 'B3'], ['B2', 'B3', 'B4'], ['C1', 'C2', 'C3'], ['C2', 'C3', 'C4'], ['D1', 'D2', 'D3'], ['D2', 'D3', 'D4'], ['A1', 'B1', 'C1'], ['B1', 'C1', 'D1'], ['A2', 'B2', 'C2'], ['B2', 'C2', 'D2'], ['A3', 'B3', 'C3'], ['B3', 'C3', 'D3'], ['A4', 'B4', 'C4'], ['B4', 'C4', 'D4']]
 end
 
 def cells
   range = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-
+  require 'pry'; binding.pry
   @player_board = {}
   range.each do |element|
     @player_board[element] = Cell.new(element)
@@ -22,7 +24,7 @@ def valid_coordinate?(cell)
 end
 
 def valid_placement?(ship, cells)
-  if ship.length == cells.length
+  if ship.length == cells.length && valid_coordinate?(cells) &&
     true
   else
     false
@@ -38,5 +40,9 @@ output = range.each do |item|
   item.split << item
   
 end
-
 p output
+
+def consecutive_coordinates(ship, coordinates)
+  @valid_cruiser.include?(coordinates) || @valid_submarine.include?(coordinates)
+end
+
