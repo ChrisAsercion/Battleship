ce520a6b2636057a03d6c585fd8dd830df209f89
pcbennett108 <pcbennett42@gmail.com>
2023-05-27 18:44:16 -0600

    Add place_cruiser partial to player_start method

--- lib/game.rb
+++ lib/game.rb
@@ -10,8 +10,8 @@ def game
     bot_board = Board.new
     bot_cruiser = Ship.new("Cruiser", 3)
     bot_submarine = Ship.new("Submarine", 2)
+
     # Making ship placement static for now until we get it working
-    # still needs validation
     bot_board.place(bot_submarine, ["C2", "D2"])
     bot_board.place(bot_cruiser, ["A1", "A2", "A3"])
     
@@ -26,14 +26,25 @@ def game
     
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
+    puts board.render
+
+
     
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    # All non-alphanumeric removed then char pairs put into array
+    cruiser_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '').scan(/../)
+    valid_coordinate = cruiser_location.all? { |cell| board.valid_coordinate?(cell) }
+    valid_placement = board.valid_placement?(cruiser, cruiser_location)
 
-  end
+    if valid_coordinate && valid_placement
+      board.place(cruiser, cruiser_location)
+    else
+      puts "Invalid Placement, please try again"
+    end
+    
+    puts board.render(true)
+end
 
 
 
-
-
-
-  puts "game is starting"
 end
