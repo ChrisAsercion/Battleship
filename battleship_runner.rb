1c95a52ae52fe2c7e4986beac4ab7a864595edc2
pcbennett108 <pcbennett42@gmail.com>
2023-05-29 14:58:25 -0600

    Add show_boards & player_shot, put Game in a class

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,3 +1,6 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
 require './lib/game'
 
 
@@ -5,9 +8,12 @@ require './lib/game'
 puts "Welcome to BATTLESHIP"
 puts "Enter p to play. Enter q to quit."
 answer = gets.chomp.downcase
+game = Game.new
+
 
 if answer == "p"
-computer_start
+game.computer_start
+
 else
   puts "Good bye"
 end
--- lib/game.rb
+++ lib/game.rb
@@ -3,61 +3,118 @@ require './lib/cell'
 require './lib/board'
 
 
-def computer_start
-  # Player and bot have their own boards
-  bot_board = Board.new
-  bot_cruiser = Ship.new("Cruiser", 3)
-  bot_submarine = Ship.new("Submarine", 2)
 
-  # Making ship placement static for now until we get it working
-  bot_board.place(bot_submarine, ["C2", "D2"])
-  bot_board.place(bot_cruiser, ["A1", "A2", "A3"])
+
+class Game
+attr_reader :bot_board,
+            :bot_cruiser,
+            :bot_submarine,
+            :board,
+            :cruiser,
+            :submarine
+
+  def initialize
+    @bot_board = Board.new
+    @bot_cruiser = Ship.new("Cruiser", 3)
+    @bot_submarine = Ship.new("Submarine", 2)
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+
+  end
+
+  def computer_start
+    # Player and bot have their own boards
+    @bot_board = Board.new
+    @bot_cruiser = Ship.new("Cruiser", 3)
+    @bot_submarine = Ship.new("Submarine", 2)
   
-  puts "I have laid out my ships on the grid."
-  player_start
-end
+    # Making ship placement static for now until we get it working
+    @bot_board.place(@bot_submarine, ["C2", "D2"])
+    @bot_board.place(@bot_cruiser, ["A1", "A2", "A3"])
+    
+    puts "I have laid out my ships on the grid."
+    player_start
+  end
 
-def player_start
-  board = Board.new
-  cruiser = Ship.new("Cruiser", 3)
-  submarine = Ship.new("Submarine", 2)
+  def player_start
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts @board.render
   
-  puts "You now need to lay out your two ships."
-  puts "The Cruiser is three units long and the Submarine is two units long."
-  puts board.render
-
-
   
-  cruiser_placed = false
-  until cruiser_placed
-  puts "Enter the squares for the Cruiser (3 spaces):"
-  # All non-alphanumeric removed then char pairs put into array
-  cruiser_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '').scan(/../)
-  valid_coordinate = cruiser_location.all? { |cell| board.valid_coordinate?(cell) }
-  valid_placement = board.valid_placement?(cruiser, cruiser_location)
-
-    if valid_coordinate && valid_placement
-      board.place(cruiser, cruiser_location)
-      cruiser_placed = true
-    else
-      puts "Invalid Placement, please try again"
+    
+    cruiser_placed = false
+    until cruiser_placed
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    # All non-alphanumeric removed then char pairs put into array
+    cruiser_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '').scan(/../)
+    valid_coordinate = cruiser_location.all? { |cell| @board.valid_coordinate?(cell) }
+    valid_placement = @board.valid_placement?(@cruiser, cruiser_location)
+  
+      if valid_coordinate && valid_placement
+        @board.place(@cruiser, cruiser_location)
+        cruiser_placed = true
+      else
+        puts "Invalid Placement, please try again"
+      end
+    end
+  
+    submarine_placed = false
+    until submarine_placed
+      puts "Enter the squares for the Submarine (2 spaces):"
+      submarine_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '').scan(/../)
+      valid_coordinate = submarine_location.all? { |cell| @board.valid_coordinate?(cell) }
+      valid_placement = @board.valid_placement?(@submarine, submarine_location)
+  
+      if valid_coordinate && valid_placement
+        place = board.place(@submarine, submarine_location)
+        if place
+          submarine_placed = true
+        else
+          puts "Ships cannot overlap each other, please try again"
+        end
+      else
+        puts "Invalid coordinates, please try again"
+      end
+    end
+    puts @board.render(true)
+    show_boards
+    player_shot
+  end
+  
+  def show_boards
+    puts "=============COMPUTER BOARD============="
+    puts @bot_board.render
+    puts "==============PLAYER BOARD=============="
+    puts @board.render(true)
+  end
+  
+  def player_shot
+    shot_placed = false
+    until shot_placed
+      puts "Enter the coordinate for your shot:"
+      #require "pry" ; binding.pry
+      shot_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '')
+        if @bot_board.valid_coordinate?(shot_location)
+          shot_placed = true
+          @bot_board.cell_group[shot_location].fire_upon
+          result = @bot_board.cell_group[shot_location].render
+          puts "Your shot on #{shot_location} was a #{result}."
+        else
+          puts "Invalid coordinates, please try again"
+        end
+      puts @bot_board.render
+      #if coord is valid then
+      # get firedupon cell and see if there is a ship there
+      # if ship then fire_upon
     end
   end
 
-  submarine_placed = false
-  until submarine_placed
-    puts "Enter the squares for the Submarine (2 spaces):"
-    submarine_location = gets.chomp.upcase.gsub(/[^0-9a-z]/i, '').scan(/../)
-    valid_coordinate = submarine_location.all? { |cell| board.valid_coordinate?(cell) }
-    valid_placement = board.valid_placement?(submarine, submarine_location)
 
-    if valid_coordinate && valid_placement
-      board.place(submarine, submarine_location)
-      submarine_placed = true
-    else
-      puts "Invalid Placement, please try again"
-    end
-  end
-  
-  puts board.render(true)
 end
+
