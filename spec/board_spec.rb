153b211a40585c3f217c2e4543ebf6d0b7ffa0c6
Chris Asercion <chris.asercion@gmail.com>
2023-06-01 11:02:21 -0600

    Removes useless tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,8 +34,6 @@ RSpec.describe Board do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
       submarine = Ship.new("Submarine", 2)
-      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(true)#we thought this should be true because the input of the user shouldn't matter if they are still consecutive.
